24ca794c84a7063acb1dccf47e1868b3f20b5f9b
sparksinfosec <sparksexec@proton.me>
2024-06-01 07:20:06 +0000

    update zip script abit migtht pull into dot file

--- zip_code_lookup.sh
+++ zip_code_lookup.sh
@@ -1,16 +1,21 @@
-#!/usr/bin/bash 
+#!/usr/bin/env bash 
 
 baseurl="https://www.city-data.com/zips"
+if [[ "$#" -eq 1 ]]; then
+    zipinput="$1"
+    zipcode=$(curl -s "$baseurl/$zipinput.html" |  grep -i '<title>' | cut -d"(" -f 2 | cut -d")" -f 1 )
+else
+    read -p "Enter in a zip (US only 5 digits): " zipinput
+    zipcode=$(curl -s "$baseurl/$zipinput.html" |  grep -i '<title>' | cut -d"(" -f 2 | cut -d")" -f 1 )
+fi
 
-read -p "Enter in a zip (US only 5 digits): " zipinput
-
-zipcode=$(curl -s "$baseurl/$zipinput.html" |  grep -i '<title>' | cut -d"(" -f 2 | cut -d")" -f 1 )
+# zipcode=$(curl -s "$baseurl/$zipinput.html" |  grep -i '<title>' | cut -d"(" -f 2 | cut -d")" -f 1 )
 filenotfound=$(curl -s "$baseurl/$zipinput.html" | grep -i '<h2>' | cut -d">" -f 2 | cut -d"<" -f 1 ) 
 
 if [[ $filenotfound = "Error 404" ]]
 then
        echo "$zipinput not found: $filenotfound"
-elif [[ -z $filenotfound ]]
+elif [[ -z $filenotfound ]] 
 then 
        echo "$zipinput is in $zipcode"
 else
